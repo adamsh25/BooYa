d4e87d5130c966c6ef3c32f0f5a77bf4c6a2fa34
beeriza <freestyle3@gmail.com>
2013-11-05 00:19:14 +0200

    Initial commit

---
+++ CameraRecorder/src/com/example/booya/recording/GameActivity.java
@@ -0,0 +1,43 @@
+package com.example.booya.recording;
+
+import com.android.camerarecorder.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+		
+		Intent intent = new Intent(GameActivity.this, RecorderService.class);
+		startService(intent);
+		
+		// Here or in Camera Recorder
+		// TODO: Replace with 
+		Button btnStop = (Button) findViewById(R.id.button1);
+		btnStop.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				Intent intent = new Intent(GameActivity.this, RecorderService.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				stopService(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game, menu);
+		return true;
+	}
+
+}
