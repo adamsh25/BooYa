f11cdd7b2f65145a31544825349f1f830eaf15b0
ronlut <ronlut@gmail.com>
2013-11-23 19:29:06 +0200

    Facebook Login working, uploading a dummy post working.

--- BooYa/src/com/example/booya/social/facebook/FacebookPublishActivity.java
+++ BooYa/src/com/example/booya/social/facebook/FacebookPublishActivity.java
@@ -1,12 +1,16 @@
 package com.example.booya.social.facebook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import com.example.booya.R;
-import com.facebook.AppEventsLogger;
-import com.facebook.Session;
-import com.facebook.UiLifecycleHelper;
+import com.facebook.*;
 import com.facebook.widget.LoginButton;
+import com.facebook.widget.WebDialog;
 
 /**
  * User: Rony
@@ -14,31 +18,121 @@ import com.facebook.widget.LoginButton;
  */
 public class FacebookPublishActivity extends Activity {
     private static final String PERMISSION = "publish_actions";
+    private static final String TAG = "FacebookPublishActivity";
+    private Button publishButton;
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 
     private UiLifecycleHelper uiHelper;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        uiHelper = new UiLifecycleHelper(this, null);
+        uiHelper = new UiLifecycleHelper(this, callback);
         uiHelper.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_facebook_publish);
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        publishButton = (Button) findViewById(R.id.publishButton);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publishFeedDialog();
+            }
+        });
     }
 
-    protected void onResume() {
+    @Override
+    public void onResume() {
         super.onResume();
         uiHelper.onResume();
-
-        // Call the 'activateApp' method to log an app event for use in analytics and advertising reporting.  Do so in
-        // the onResume methods of the primary Activities that an app may be launched into.
-        AppEventsLogger.activateApp(this);
-
-        updateUI();
     }
 
-    private void updateUI() {
-        Session session = Session.getActiveSession();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            publishButton.setVisibility(View.VISIBLE);
+            Log.i(TAG, "Logged in...");
+        } else if (state.isClosed()) {
+            publishButton.setVisibility(View.INVISIBLE);
+            Log.i(TAG, "Logged out...");
+        }
+    }
+
+    private void publishFeedDialog() {
+        Bundle params = new Bundle();
+        params.putString("name", "Facebook SDK for Android");
+        params.putString("caption", "Build great social apps and get more installs.");
+        params.putString("description", "The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
+        params.putString("link", "https://developers.facebook.com/android");
+        params.putString("picture", "https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
+
+        WebDialog feedDialog = (
+                new WebDialog.FeedDialogBuilder(this,
+                        Session.getActiveSession(),
+                        params))
+                .setOnCompleteListener(new WebDialog.OnCompleteListener() {
+
+                    @Override
+                    public void onComplete(Bundle values,
+                                           FacebookException error) {
+                        if (error == null) {
+                            // When the story is posted, echo the success
+                            // and the post Id.
+                            final String postId = values.getString("post_id");
+                            if (postId != null) {
+                                Toast.makeText(getBaseContext(),
+                                        "Posted story, id: " + postId,
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                // User clicked the Cancel button
+                                Toast.makeText(getBaseContext(),
+                                        "Publish cancelled",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (error instanceof FacebookOperationCanceledException) {
+                            // User clicked the "x" button
+                            Toast.makeText(getBaseContext(),
+                                    "Publish cancelled",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Generic, ex: network error
+                            Toast.makeText(getBaseContext(),
+                                    "Error posting story",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                })
+                .build();
+        feedDialog.show();
     }
 }
