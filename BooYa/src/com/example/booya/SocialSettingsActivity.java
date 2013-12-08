c19bd52139ab40b13bf13fe5b55cb77821bdc7c8
ronlut <ronlut@gmail.com>
2013-12-08 20:55:58 +0200

    Added social settings activity

---
+++ BooYa/src/com/example/booya/SocialSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.booya;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ronlut on 08/12/13.
+ */
+public class SocialSettingsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_settings);
+    }
+}
