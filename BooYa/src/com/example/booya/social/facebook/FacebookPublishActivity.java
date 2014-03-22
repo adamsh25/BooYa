94613454cf42892c3a43f43ee7a48229ef9c0e0f
Rony Lutsky <ronlut@gmail.com>
2014-03-22 20:00:15 +0200

    WIP

--- BooYa/src/com/example/booya/social/facebook/FacebookPublishActivity.java
+++ BooYa/src/com/example/booya/social/facebook/FacebookPublishActivity.java
@@ -4,13 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.example.booya.R;
 import com.sromku.simple.fb.Permission;
 import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.entities.Privacy;
 import com.sromku.simple.fb.entities.Video;
 import com.sromku.simple.fb.listeners.OnLoginListener;
 import com.sromku.simple.fb.listeners.OnLogoutListener;
@@ -27,25 +25,20 @@ public class FacebookPublishActivity extends Activity {
 
     private SimpleFacebook mSimpleFacebook;
 
-    private Button mButtonLogin;
-    private Button mButtonLogout;
-    private TextView mTextStatus;
-    private Button mButtonPublish;
-
     // Login listener
     private OnLoginListener mOnLoginListener = new OnLoginListener()
     {
         @Override
         public void onFail(String reason)
         {
-            mTextStatus.setText(reason);
+            toast(reason);
             Log.w(TAG, "Failed to login");
         }
 
         @Override
         public void onException(Throwable throwable)
         {
-            mTextStatus.setText("Exception: " + throwable.getMessage());
+            toast("Exception: " + throwable.getMessage());
             Log.e(TAG, "Bad thing happened", throwable);
         }
 
@@ -53,20 +46,19 @@ public class FacebookPublishActivity extends Activity {
         public void onThinking()
         {
             // show progress bar or something to the user while login is happening
-            mTextStatus.setText("Thinking...");
+            toast("Thinking...");
         }
 
         @Override
         public void onLogin()
         {
             // change the state of the button or do whatever you want
-            mTextStatus.setText("Logged in");
-            loggedInUIState();
+            toast("Logged in");
         }
 
         @Override
         public void onNotAcceptingPermissions(Permission.Type type) {
-            toast("You didn't accept read permissions");
+            toast(String.format("You didn't accept %s permissions", type.name()));
         }
     };
 
@@ -77,14 +69,14 @@ public class FacebookPublishActivity extends Activity {
         @Override
         public void onFail(String reason)
         {
-            mTextStatus.setText(reason);
+            toast(reason);
             Log.w(TAG, "Failed to login");
         }
 
         @Override
         public void onException(Throwable throwable)
         {
-            mTextStatus.setText("Exception: " + throwable.getMessage());
+            toast("Exception: " + throwable.getMessage());
             Log.e(TAG, "Bad thing happened", throwable);
         }
 
@@ -92,15 +84,14 @@ public class FacebookPublishActivity extends Activity {
         public void onThinking()
         {
             // show progress bar or something to the user while login is happening
-            mTextStatus.setText("Thinking...");
+            toast("Thinking...");
         }
 
         @Override
         public void onLogout()
         {
             // change the state of the button or do whatever you want
-            mTextStatus.setText("Logged out");
-            loggedOutUIState();
+            toast("Logged out");
             toast("You are logged out");
         }
 
@@ -110,22 +101,12 @@ public class FacebookPublishActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_facebook_publish);
-
-        initUI();
-
-        mButtonPublish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                publishDummyVideo();
-            }
-        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
         mSimpleFacebook = SimpleFacebook.getInstance(this);
-        setUIState();
     }
 
     @Override
@@ -136,10 +117,10 @@ public class FacebookPublishActivity extends Activity {
     }
 
     private void publishDummyVideo() {
+
         // create publish listener
         OnPublishListener onPublishListener = new OnPublishListener()
         {
-
             @Override
             public void onFail(String reason)
             {
@@ -167,49 +148,18 @@ public class FacebookPublishActivity extends Activity {
             }
         };
 
+        Privacy privacy = new Privacy.Builder().setPrivacySettings(Privacy.PrivacySettings.SELF).build();
+
         // create Video instace and add some properties
         Video videoObj = new Video.Builder()
         .setVideo(new File("/sdcard/dummy.mp4"))
         .setDescription("Dummy Description #hashtag")
-        .setName("Dummy Title")
+        .setName("Dummy Title") //title
+        .setPrivacy(privacy)
         .build();
 
         // publish video to Videos album
         mSimpleFacebook.publish(videoObj, onPublishListener);
-    }
-
-    private void initUI()
-    {
-        mTextStatus = (TextView)findViewById(R.id.text_status);
-        mButtonPublish = (Button)findViewById(R.id.publishButton);
-    }
-
-    private void setUIState()
-    {
-        if (mSimpleFacebook.isLogin())
-        {
-            loggedInUIState();
-        }
-        else
-        {
-            loggedOutUIState();
-        }
-    }
-
-    private void loggedInUIState()
-    {
-        //mButtonLogin.setEnabled(false);
-        //mButtonLogout.setEnabled(true);
-        mButtonPublish.setEnabled(true);
-        mTextStatus.setText("Logged in");
-    }
-
-    private void loggedOutUIState()
-    {
-        //mButtonLogin.setEnabled(true);
-        //mButtonLogout.setEnabled(false);
-        mButtonPublish.setEnabled(false);
-        mTextStatus.setText("Logged out");
     }
 
     private void toast(String message)
